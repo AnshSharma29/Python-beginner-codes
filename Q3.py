ae2ff2de3bcb78875b4d432ff9e9af7a342c1632
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2023-08-12 11:11:01 +0530

    Update Q3.py

--- Q3.py
+++ Q3.py
@@ -1,5 +1,9 @@
 # Write a Python program to accept a string from user and find out the length of the given string.
 
 a=input("Enter the string:")
+
 print("The length of the string is: ",len(a))
+
+
+
 
