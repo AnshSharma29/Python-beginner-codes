527f11d08967e97a06a2d39cfa88aa1d12d55266
Ansh Sharma <115182995+AnshSharma29@users.noreply.github.com>
2023-08-13 11:17:33 +0530

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -5,6 +5,7 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScalar, normalize, Binarizer, scale
 
 df=pd.DataFrame(np.random.randn(4,3),
+
 index=['a','b','c','d'],
 columns=['C1','C2','C3'])
 
@@ -12,11 +13,13 @@ print("Original Dataset: ")
 print(df)
 
 df1=df.copy()
+
 scalar=MinMxScalar(feature_range=(0,1))
 rescaled_data = scalar.fit_transform(df1)
 print(rescaled_data)
 
 df2=df.copy()
+
 normalize_data=normalize(df,norm='l1')
 print(normalize_data)
 
